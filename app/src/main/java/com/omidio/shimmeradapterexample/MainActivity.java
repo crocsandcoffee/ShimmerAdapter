ea82b8347dbefd5d64e789e7751f79e2f5b40ced
Omid Ghenatnevi <O.Ghenatnevi@gmail.com>
2019-12-17 11:00:15 -0500

    Update demo

--- app/src/main/java/com/omidio/shimmeradapterexample/MainActivity.java
+++ app/src/main/java/com/omidio/shimmeradapterexample/MainActivity.java
@@ -24,6 +24,6 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 adapter.setShowShimmer(false);
             }
-        }, 6000);
+        }, 8000);
     }
 }
